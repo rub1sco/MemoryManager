f30a709749b6f543e7d554141fe93d34dced2be6
rub1sco <35971184+rub1sco@users.noreply.github.com>
2020-04-28 18:14:13 -0600

    fixing depencies

---
+++ MemoryManager/MemoryManager/functions.h
@@ -0,0 +1,41 @@
+#ifndef functions_h
+#define functions_h
+#include <tuple>
+#include <ctime>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+#include <random>
+#include <fstream>
+
+#include "victimPage.h"
+#include "TLB.h"
+#include "pageTable.h"
+
+
+#define MAX_LOGICAL_ADDR 65535
+//typedef unsigned char Byte;
+
+using namespace std;
+
+struct Application {
+    int frame;
+    string appName;
+};
+
+
+//definitons of helper functions
+int randomNumberGenerator();
+tuple<int,int> parser(int logicalAddress, int NumPages);
+vector<victimPage> updateTally(unordered_map<int, int> TLB, int cpu_pg, vector<victimPage> & TLBVec, bool firstOccurrence);
+TLB replacePage (vector<victimPage> & TLBVec, int pageNumber, pageTable pageTable,  TLB TLB, Application backingStore[]);
+void replacePageV2(vector<victimPage> & TLBVec, int pageNumber, pageTable pageTable,  TLB TLB, Application backingStore[]);
+void mainMenu();
+void locateRandomAddress(Application backingStore[]);
+void locateKnownAddress(Application backingStore[]);
+void decisionMaker(tuple<int,int> physTranslation, TLB TLB, pageTable pageTable, vector<victimPage> & TLBVec, Application backingStore[]);
+void buildBackingStore(Application backingStore[]);
+
+#endif /* functions_h */
